0ac7ca39502a8d141cb5f5498c56d806447b0b72
Rayanderson Sousa <rayandersonsousa@pop-os.localdomain>
2023-03-12 17:26:41 -0300

    feat: Desenvolvido testes que cobrem 80% dos arquivos

--- app/backend/src/integration/ultils/macthMock.ts
+++ app/backend/src/integration/ultils/macthMock.ts
@@ -1,4 +1,4 @@
-const matches = [
+export const matches = [
   {
     id: 41,
     homeTeamId: 16,
@@ -43,4 +43,63 @@ const matches = [
   },
 ];
 
-export default matches;
+export const finishedMatches = [
+  {
+    id: 1,
+    homeTeamId: 2,
+    homeTeamGoals: 1,
+    awayTeamId: 8,
+    awayTeamGoals: 1,
+    inProgress: 0,
+    homeTeam: {
+      teamName: 'São Paulo',
+    },
+    awayTeam: {
+      teamName: 'Grêmio',
+    },
+  },
+  {
+    id: 2,
+    homeTeamId: 9,
+    homeTeamGoals: 1,
+    awayTeamId: 14,
+    awayTeamGoals: 1,
+    inProgress: 0,
+    homeTeam: {
+      teamName: 'Internacional',
+    },
+    awayTeam: {
+      teamName: 'Santos',
+    },
+  },
+  {
+    id: 3,
+    homeTeamId: 4,
+    homeTeamGoals: 3,
+    awayTeamId: 11,
+    awayTeamGoals: 0,
+    inProgress: 0,
+    homeTeam: {
+      teamName: 'Corinthians',
+    },
+    awayTeam: {
+      teamName: 'Napoli-SC',
+    },
+  },
+];
+
+export const newMatch = {
+  homeTeamId: 7,
+  awayTeamId: 4,
+  homeTeamGoals: 6,
+  awayTeamGoals: 1,
+};
+
+export const outputMatch = {
+  id: 1,
+  homeTeamId: 7,
+  homeTeamGoals: 6,
+  awayTeamId: 4,
+  awayTeamGoals: 1,
+  inProgress: true,
+};
--- app/backend/src/tests/Matches.test.ts
+++ app/backend/src/tests/Matches.test.ts
@@ -4,7 +4,7 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import matches from '../integration/ultils/macthMock';
+import { matches, finishedMatches, newMatch, outputMatch } from '../integration/ultils/macthMock';
 import Matches from '../database/models/Matches';
 import { IMatch } from '../interfaces/IMatch';
 
@@ -24,5 +24,55 @@ describe('Testes de integração do endpoit /matches', function() {
 
     expect(response.status).to.be.equal(200);
     expect(response.body).to.be.deep.equal(matches);
+  })
+
+  it('Testa se é possível requisitar as matches em progresso', async function () {
+    sinon.stub(Matches, 'findAll').resolves(matches as unknown as IMatch[]);
+
+    const response = await chai.request(app).get('/matches?inProgress=true');
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.be.deep.equal(matches);
+  })
+
+  it('Testa se é possível requisitar as matches que já terminaram', async function() {
+    sinon.stub(Matches, 'findAll').resolves(finishedMatches as unknown as IMatch[]);
+
+    const response = await chai.request(app).get('/matches?inProgress=false');
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.be.deep.equal(finishedMatches);
+  })
+
+  it('Testa se é possível atualizar uma partida em andamento', async function() {
+    const loginData = { email: 'user@user.com', password: 'secret_user' };
+    const uLogin = await chai.request(app).post('/login').send(loginData);
+
+    expect(uLogin.body.token).to.not.be.empty;
+
+    const token:string = uLogin.body.token;
+    
+    sinon.stub(Matches, 'update').resolves()
+
+    const response = await chai.request(app).patch('matches/1/finish').set('authorization', token)
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.be.deep.equal({ message: 'Daijoubu' });
+  })
+
+  it('Testa se é possível cadastrar uma nova partida', async function() {
+    const loginData = { email: 'user@user.com', password: 'secret_user' };
+    const uLogin = await chai.request(app).post('/login').send(loginData);
+
+    expect(uLogin.body.token).to.not.be.empty;
+
+    const token:string = uLogin.body.token;
+
+    sinon.stub(Matches, 'create').resolves(outputMatch as unknown as IMatch)
+
+    const response = await chai.request(app).post('/matches').send(newMatch).set('authorization', token)
+
+    expect(response.status).to.be.equal(201);
+    expect(response.body).to.be.deep.equal(outputMatch);
   })
 })
